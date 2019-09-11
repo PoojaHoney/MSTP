34b2c6ed54cd5af803b50327eb5fc31f18b3a7c5
Pooja-Honey <42027547+PoojaHoney@users.noreply.github.com>
2019-09-11 16:15:24 +0530

    Add files via upload

---
+++ 9module1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def function(name):
+    return "Welcome "+name+' have a nice day'
+
+
+# In[2]:
+
+
+def sub(a,b):
+    return a-b
+
+
+# In[3]:
+
+
+def mul(a,b):
+    return a*b
+
+
+# In[4]:
+
+
+def div(a,b):
+    if b==0:
+        return "Division by zero Error"
+    else:
+        return a//b
+
+
+# In[ ]:
+
+
+
+def add(a,b):
+    return a+b
+
